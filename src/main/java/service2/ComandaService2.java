c68f16c9bb49953641fd1fd5b806329fbf57fa0f
Karine <contato.karinecostap@gmail.com>
2025-09-26 11:57:50 -0300

    Adição da funcionalidade de fechar comanda

--- src/main/java/service2/ComandaService2.java
+++ src/main/java/service2/ComandaService2.java
@@ -1,5 +1,8 @@
 package service2;
 
+import model.Comanda;
+import model.Produto;
+
 import java.util.Scanner;
 
 public class ComandaService2 {
@@ -10,4 +13,17 @@ public class ComandaService2 {
         Integer numeroDigitado = scanner.nextInt();
         return numeroDigitado;
     }
+    public Comanda fecharComanda(Comanda comanda) {
+        System.out.println("Fechando comanda da mesa " + comanda.getMesa() + ":");
+        double total = 0.0;
+        for (Produto produto : comanda.getProdutos()) {
+            double valorProduto = produto.getPrecoInteiro() * produto.getQuantidade();
+            System.out.println(produto.getQuantidade() + " x  " + produto.getNomeProduto() + " | Preço: R$ " + valorProduto);
+            total += valorProduto;
+        }
+        System.out.println("Valor total a pagar: R$ " + total);
+        comanda.setStatus("FECHADA");
+        return comanda;
+    }
+
 }
